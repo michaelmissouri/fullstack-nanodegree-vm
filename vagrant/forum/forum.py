a917886b11c29b9492ae470dd9946bccc1ee6fce
Michael Missouri <michaelmissouri@gmail.com>
2016-03-02 20:37:46 -0500

    Just making sure this actually works

--- vagrant/forum/forum.py
+++ vagrant/forum/forum.py
@@ -2,6 +2,8 @@
 # DB Forum - a buggy web forum server backed by a good database
 #
 
+# Just making sure this works
+
 # The forumdb module is where the database interface code goes.
 import forumdb
 
